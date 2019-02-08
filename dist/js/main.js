a34d2f1365995c8c3e0e15140659c848a4aaca73
David Salazar <david.asal@hotmail.com>
2019-02-08 13:36:24 -0600

    Added Links and finished home page

---
+++ dist/js/main.js
@@ -0,0 +1,68 @@
+// Scroll Magic Slides
+var slides = new ScrollMagic.Controller();
+
+$('.col-md-6').each(function () {
+  var cardSlide = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.7
+    })
+    .setClassToggle(this, 'card-appear')
+
+    .addTo(slides);
+});
+
+// Clickable cards that go to different links
+
+document.getElementById("wmod").onclick = function () {
+  location.href = "wealth-mods-101.html";
+};
+document.getElementById("rmod").onclick = function () {
+  location.href = "retirement-mod-calculator";
+};
+document.getElementById("rskmod").onclick = function () {
+  location.href = "risk-profile-mod-calculator.html";
+};
+document.getElementById("fmod").onclick = function () {
+  location.href = "financial-planning-mods.html";
+};
+document.getElementById("armismod").onclick = function () {
+  location.href = "https://armisfinancial.com";
+};
+
+// Scoll Magic Smooth Scrolling
+var controller = new ScrollMagic.Controller();
+
+var tween = TweenMax.from("#sec2", 0.9, {
+  autoAlpha: 0,
+  scale: 0.7
+});
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#discover",
+    duration: 500,
+    triggerHook: .5
+  })
+  .setTween(tween)
+
+  .addTo(controller);
+
+controller.scrollTo(function (newpos) {
+  TweenMax.to(window, 1, {
+    scrollTo: {
+      y: newpos
+    }
+  });
+});
+
+$(document).on("click", "a[href^='#']", function (e) {
+  var id = $(this).attr("href");
+  if ($(id).length > 0) {
+    e.preventDefault();
+
+    controller.scrollTo(id);
+
+    if (window.history && window.history.pushState) {
+      history.pushState("", document.title, id);
+    }
+  }
+});
