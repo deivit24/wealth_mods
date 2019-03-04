8157d30e02b05fd490af67f65d6c01fb3bcfd246
David Salazar <david.asal@hotmail.com>
2019-03-04 11:03:47 -0600

    added about pic logo white and black and animation

--- dist/js/about.js
+++ dist/js/about.js
@@ -9,3 +9,35 @@ document.getElementById("tool").onclick = function () {
 document.getElementById("blog").onclick = function () {
   location.href = "mods/wealth-mods-101.html";
 };
+
+
+var nav = new ScrollMagic.Controller();
+
+var navscene = new ScrollMagic.Scene({
+    triggerElement: ".jumbotron h1",
+    triggerHook: .1
+  })
+  .setClassToggle(".navbar", "bg-white")
+  .addIndicators()
+  .addTo(nav);
+
+
+var black = new ScrollMagic.Controller();
+
+var blacknav = new ScrollMagic.Scene({
+    triggerElement: ".jumbotron h1",
+    triggerHook: .1
+  })
+  .setClassToggle("#black", "appear")
+  .addIndicators()
+  .addTo(black);
+
+var white = new ScrollMagic.Controller();
+
+var whitenav = new ScrollMagic.Scene({
+    triggerElement: ".jumbotron h1",
+    triggerHook: .1
+  })
+  .setClassToggle("#white", "invisible")
+  .addIndicators()
+  .addTo(white);
