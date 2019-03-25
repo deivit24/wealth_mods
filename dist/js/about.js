60f843f842bbd3af8956c0169f789bcd86b5ff38
David Salazar <david.asal@hotmail.com>
2019-03-25 07:01:04 -0500

    added new links and new slide to transition to new section

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,56 +1,52 @@
 // making cards in start here page a link!
 
-document.getElementById("about").onclick = function () {
-  location.href = "about.html";
+document.getElementById('about').onclick = function() {
+  location.href = 'about.html';
 };
-document.getElementById("tool").onclick = function () {
-  location.href = "tool.html";
+document.getElementById('tool').onclick = function() {
+  location.href = 'tool.html';
 };
-document.getElementById("blog").onclick = function () {
-  location.href = "mods/wealth-mods-101.html";
+document.getElementById('blog').onclick = function() {
+  location.href = 'mods/wealth-mods-101.html';
 };
-
 
 var nav = new ScrollMagic.Controller();
 
 var navscene = new ScrollMagic.Scene({
-    triggerElement: ".jumbotron h1",
-    triggerHook: .1
-  })
-  .setClassToggle(".navbar", "bg-white")
+  triggerElement: '.jumbotron h1',
+  triggerHook: 0.1
+})
+  .setClassToggle('.navbar', 'bg-white')
 
   .addTo(nav);
-
 
 var black = new ScrollMagic.Controller();
 
 var blacknav = new ScrollMagic.Scene({
-    triggerElement: ".jumbotron h1",
-    triggerHook: .1
-  })
-  .setClassToggle("#black", "appear")
+  triggerElement: '.jumbotron h1',
+  triggerHook: 0.1
+})
+  .setClassToggle('#black', 'appear')
 
   .addTo(black);
 
 var white = new ScrollMagic.Controller();
 
 var whitenav = new ScrollMagic.Scene({
-    triggerElement: ".jumbotron h1",
-    triggerHook: .1
-  })
-  .setClassToggle("#white", "invisible")
+  triggerElement: '.jumbotron h1',
+  triggerHook: 0.1
+})
+  .setClassToggle('#white', 'invisible')
 
   .addTo(white);
 
-
 var navcolors = new ScrollMagic.Controller();
 
-
-$('.nav-link').each(function () {
+$('.nav-link').each(function() {
   var navColors = new ScrollMagic.Scene({
-      triggerElement: ".jumbotron h1",
-      triggerHook: 0.0
-    })
+    triggerElement: '.jumbotron h1',
+    triggerHook: 0.0
+  })
     .setClassToggle(this, 'dark')
 
     .addTo(navcolors);
@@ -59,10 +55,10 @@ $('.nav-link').each(function () {
 var aboutActive = new ScrollMagic.Controller();
 
 var aboutActivenav = new ScrollMagic.Scene({
-    triggerElement: ".jumbotron h1",
-    triggerHook: .0
-  })
-  .setClassToggle(".about-active", "about-active-dark")
+  triggerElement: '.jumbotron h1',
+  triggerHook: 0.0
+})
+  .setClassToggle('.about-active', 'about-active-dark')
 
   .addTo(aboutActive);
 
@@ -71,10 +67,10 @@ var aboutActivenav = new ScrollMagic.Scene({
 var why = new ScrollMagic.Controller();
 
 var aboutwhy = new ScrollMagic.Scene({
-    triggerElement: "#why",
-    triggerHook: .8
-  })
-  .setClassToggle("#why", "why-appear")
+  triggerElement: '#why',
+  triggerHook: 0.8
+})
+  .setClassToggle('#why', 'why-appear')
 
   .addTo(why);
 
@@ -83,19 +79,31 @@ var aboutwhy = new ScrollMagic.Scene({
 var confused = new ScrollMagic.Controller();
 
 var aboutconfused = new ScrollMagic.Scene({
-    triggerElement: "#why",
-    triggerHook: .8
-  })
-  .setClassToggle("#sec7 img", "why-appear")
+  triggerElement: '#why',
+  triggerHook: 0.8
+})
+  .setClassToggle('#sec7 img', 'why-appear')
 
   .addTo(confused);
 
 var confusedWords = new ScrollMagic.Controller();
 
 var aboutconfusedWords = new ScrollMagic.Scene({
-    triggerElement: "#why",
-    triggerHook: .8
-  })
-  .setClassToggle("#sec7 .col-md-8", "why-appear")
+  triggerElement: '#why',
+  triggerHook: 0.8
+})
+  .setClassToggle('#sec7 .col-md-8', 'why-appear')
 
   .addTo(confusedWords);
+
+// Why I hated it
+
+var hated = new ScrollMagic.Controller();
+
+var abouthated = new ScrollMagic.Scene({
+  triggerElement: '#hated',
+  triggerHook: 0.8
+})
+  .setClassToggle('#hated', 'why-appear')
+  .addIndicators()
+  .addTo(hated);
