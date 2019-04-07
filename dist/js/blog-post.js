1272b48b7080b8a046908ee6bd620ff8f211de6f
David Salazar <david.asal@hotmail.com>
2019-04-06 21:04:53 -0500

    added animation to pagnation

--- dist/js/blog-post.js
+++ dist/js/blog-post.js
@@ -3,13 +3,15 @@ var pinning = new ScrollMagic.Controller();
 $(function () {
   // build a pinning scence
   var pin1 = new ScrollMagic.Scene({
-      triggerElement: ".sub",
+      triggerElement: ".breadcrumbs",
       duration: 0,
-      triggerHook: .1
+      triggerHook: 0,
+      offset: -121.45
 
     })
 
     .setPin(".breadcrumbs")
+    .setClassToggle(".breadcrumbs", "white")
     .addIndicators()
     .addTo(pinning);
 
