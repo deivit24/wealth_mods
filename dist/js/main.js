9e0d583d8d8e5140a8fe792e94ad974fb1b7aafe
David Salazar <david.asal@hotmail.com>
2019-04-20 18:42:05 -0500

    changed locations

--- dist/js/main.js
+++ dist/js/main.js
@@ -64,16 +64,16 @@ var aboutsocial = new ScrollMagic.Scene({
 // Clickable cards that go to different links
 
 document.getElementById("wmod").onclick = function () {
-  location.href = "mods/wealth-mods-101.html";
+  location.href = "wealth-mods-101.html";
 };
 document.getElementById("rmod").onclick = function () {
-  location.href = "mods/retirement-mod-calculator.html";
+  location.href = "retirement-mod-calculator.html";
 };
 document.getElementById("rskmod").onclick = function () {
-  location.href = "mods/risk-profile-mod-calculator.html";
+  location.href = "risk-profile-mod-calculator.html";
 };
 document.getElementById("fmod").onclick = function () {
-  location.href = "mods/financial-planning-mods.html";
+  location.href = "financial-planning-mods.html";
 };
 document.getElementById("armismod").onclick = function () {
   location.href = "https://armisfinancial.com";
