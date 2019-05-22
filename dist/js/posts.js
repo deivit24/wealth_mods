410a9d2334ee3360f855f969f4b395e1e583f9fd
David Salazar <david.asal@hotmail.com>
2019-05-22 12:22:39 -0500

    add 401k mod

--- dist/js/posts.js
+++ dist/js/posts.js
@@ -1,7 +1,19 @@
-let past = document.querySelector(" #blogs");
+let past = document.querySelector(' #blogs');
 
-let post = [{
-
+let post = [
+  {
+    title: 'The 401(k) MOD 2019',
+    tag: 'Wealth MODs 101',
+    date: 'May 22nd 2019',
+    sub: 'A complete guide',
+    src: 'dist/img/blog/401k.jpg',
+    author: 'David Salazar',
+    link: 'wealth-mods-101/401k-MOD-2019.html',
+    preview:
+      'A 401(k) is a retirement plan that is provided to you by your employer. It allows employees to save a piece of their paycheck and invest it before taxes are taken out. You are hence...',
+    class: 'blog-card alt'
+  },
+  {
     title: 'The Roth IRA MOD 2019',
     tag: 'Wealth MODs 101',
     date: 'May 16th 2019',
@@ -9,11 +21,11 @@ let post = [{
     src: 'dist/img/blog/retirement1.jpg',
     author: 'David Salazar',
     link: 'wealth-mods-101/Roth-IRA-MOD-2019.html',
-    preview: 'The Roth IRA is a savings vehicle that is designed to help you prepare for one of the most challenging financial goals you can have - retirement. It’s an investment vehicle that...',
+    preview:
+      'The Roth IRA is a savings vehicle that is designed to help you prepare for one of the most challenging financial goals you can have - retirement. It’s an investment vehicle that...',
     class: 'blog-card'
   },
   {
-
     title: 'The Retirement MOD Calculator',
     tag: 'Tools',
     date: 'May 10th 2019',
@@ -21,11 +33,11 @@ let post = [{
     src: 'dist/img/blog/calculator.jpg',
     author: 'David Salazar',
     link: 'tools/retirement-mod-calculator.html',
-    preview: 'Time and time and again, research from Morningstar shows that planning for "retirement" is still the top financial goal for an individual. As a matter of fact, planning for retirement is... ',
+    preview:
+      'Time and time and again, research from Morningstar shows that planning for "retirement" is still the top financial goal for an individual. As a matter of fact, planning for retirement is... ',
     class: 'blog-card alt'
   },
   {
-
     title: 'The Traditional IRA MOD 2019',
     tag: 'Wealth MODs 101',
     date: 'May 4th 2019',
@@ -33,38 +45,37 @@ let post = [{
     src: 'dist/img/blog/retirement2.jpg',
     author: 'David Salazar',
     link: 'wealth-mods-101/Traditional-IRA-MOD-2019.html',
-    preview: 'There are many investment and savings vehicles out there that are designed to help you prepare for one of the most challenging financial goals you can have - retirement. You can save... ',
+    preview:
+      'There are many investment and savings vehicles out there that are designed to help you prepare for one of the most challenging financial goals you can have - retirement. You can save... ',
     class: 'blog-card'
   },
   {
-
     title: '3 Millennial Money Habits Stereotypes That Have Been Debunked ',
     tag: 'Wealth MODs 101',
     date: 'April 27th, 2019',
     sub: 'But We Can Do Better ',
     src: 'dist/img/blog/young2.jpg',
     author: 'David Salazar',
-    link: 'wealth-mods-101/3-Millennial-Money-Habits-Stereotypes-That-Have-Been-Debunked.html',
-    preview: '“Foolish when it comes to money,” “They don’t plan for the long-term,” “They are self-centered,” “They still depend on their parents.” Those are the typical stereotypes... ',
+    link:
+      'wealth-mods-101/3-Millennial-Money-Habits-Stereotypes-That-Have-Been-Debunked.html',
+    preview:
+      '“Foolish when it comes to money,” “They don’t plan for the long-term,” “They are self-centered,” “They still depend on their parents.” Those are the typical stereotypes... ',
     class: 'blog-card alt'
   },
   {
-
     title: '3 Main Differences Between Roth IRA and Traditional IRA 2019',
     tag: 'Wealth MODs 101',
     date: 'April 20th, 2019',
     sub: 'Which Should Millennials Choose?',
     src: 'dist/img/blog/young1.jpg',
     author: 'David Salazar',
-    link: 'wealth-mods-101/3-Main-Differences-Between-Roth-IRA-and-Traditional-IRA-2019.html',
-    preview: 'Ever since I can remember, I would listen to some guy in a suit on the news mentioned the word IRA. I would hear it from my parents every once in a while, talk about it as ...',
+    link:
+      'wealth-mods-101/3-Main-Differences-Between-Roth-IRA-and-Traditional-IRA-2019.html',
+    preview:
+      'Ever since I can remember, I would listen to some guy in a suit on the news mentioned the word IRA. I would hear it from my parents every once in a while, talk about it as ...',
     class: 'blog-card '
   }
-
-
 ];
-
-
 
 function allPost() {
   let output = ' ';
@@ -73,7 +84,9 @@ function allPost() {
     output += `
     <div class="${post[i].class}">
             <div class="meta">
-              <div class="photo" style="background-image: url(${post[i].src})"></div>
+              <div class="photo" style="background-image: url(${
+                post[i].src
+              })"></div>
               <ul class="details">
                 <li class="author">
                   <i class="fas fa-user"></i> ${post[i].author}
@@ -105,14 +118,14 @@ function allPost() {
 }
 allPost();
 
-// Navigation Image Slide 
+// Navigation Image Slide
 
 var imageSlideSmall = new ScrollMagic.Controller();
 
 var imageSM = new ScrollMagic.Scene({
-    triggerElement: 'section',
-    triggerHook: 0
-  })
+  triggerElement: 'section',
+  triggerHook: 0
+})
   .setClassToggle('.smallImage', 'd-appear')
 
   .addTo(imageSlideSmall);
@@ -120,14 +133,13 @@ var imageSM = new ScrollMagic.Scene({
 var imageSlideBig = new ScrollMagic.Controller();
 
 var imageSM = new ScrollMagic.Scene({
-    triggerElement: 'section',
-    triggerHook: 0
-  })
+  triggerElement: 'section',
+  triggerHook: 0
+})
 
   .setClassToggle('.bigImage', 'd-zero')
 
   .addTo(imageSlideBig);
-
 
 // var signpin = new ScrollMagic.Controller();
 
@@ -142,7 +154,6 @@ var imageSM = new ScrollMagic.Scene({
 //     })
 
 //     .setPin(".signup-card")
-
 
 //     .addTo(signpin);
 
